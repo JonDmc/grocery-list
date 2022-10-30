2743ee314a1738322d10c1dffe14045cb2e8fb2a
Jon Dimaculangan <jondmclngn@gmail.com>
2022-10-29 19:25:25 -0500

    category updated

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
     category: ''
   })
   const [list, setList] = useState([])
-  // const [itemList, setItemList] = useState([])
   const [catList, setCatList] = useState([
     {
       'value': 1,
@@ -22,7 +21,55 @@ function App() {
     },
     {
       'value': 2,
+      'title': 'Baking'
+    },
+    {
+      'value': 3,
       'title': 'Beverage'
+    },
+    {
+      'value': 4,
+      'title': 'Breakfast & Cereal'
+    },
+    {
+      'value': 5,
+      'title': 'Condiments & Spices'
+    },
+    {
+      'value': 6,
+      'title': 'Cookies, Candy & Snacks'
+    },
+    {
+      'value': 7,
+      'title': 'Frozen Foods'
+    },
+    {
+      'value': 8,
+      'title': 'Fruits & Vegetables'
+    },
+    {
+      'value': 9,
+      'title': 'Canned Goods & Soups'
+    },
+    {
+      'value': 10,
+      'title': 'Meat & Seafood'
+    },
+    {
+      'value': 11,
+      'title': 'Personal Care & Health'
+    },
+    {
+      'value': 12,
+      'title': 'Paper, Cleaning & Home'
+    },
+    {
+      'value': 13,
+      'title': 'Pet Care'
+    },
+    {
+      'value': 14,
+      'title': 'Wine, Beer & Spirits'
     }
   ])
 
